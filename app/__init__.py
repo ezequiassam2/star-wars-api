99069d87fd49ef20210695a715170b0c0eb7ddef
ezequias.ferreira <ezequias.ferreira@ifood.com.br>
2024-11-20 14:17:54 -0300

    Add initialize mongo

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from app.infrastructure.schemas import initialize_db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    initialize_db(app)
     return app
